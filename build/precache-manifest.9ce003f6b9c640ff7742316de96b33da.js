69807dec8e8dac7d4b9bcb4c88748a232cbceec7
A <avilavate@live.in>
2020-01-16 17:39:10 +0530

    Redesigned About Component page

---
+++ build/precache-manifest.9ce003f6b9c640ff7742316de96b33da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccf285b9195f9a6d1bdbb148f5c6c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd5175d7644d38cf8382",
+    "url": "/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "cd5175d7644d38cf8382",
+    "url": "/static/js/2.33f96d64.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.33f96d64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a39e70eaa6f7eeb8a5e2",
+    "url": "/static/js/main.220873c4.chunk.js"
+  },
+  {
+    "revision": "b64d892d9e1d19a7dca1",
+    "url": "/static/js/runtime-main.da24ab99.js"
+  }
+]);
--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 class AboutPage extends React.Component {
+  style = { maxHight: "300px", paddingBottom: "50px" };
   render() {
     return (
       <>
-        <h1>About</h1>
-        <p>This is About page</p>
+        <div className="card">
+          <div className="card-header">About</div>
+          <div className="card-body">
+            <div className="container-fluid">
+              <div className="text-center">
+                <img
+                  src={require("../assets/images/avilavate.jpg")}
+                  className="img-responsive center-block"
+                  style={this.style}
+                  alt="Avil Avate"
+                />
+              </div>
+
+              <div className="text-center">Developed by:</div>
+              <div className="text-center">
+                <h3>Avil Avate</h3>
+              </div>
+
+              <div className="text-center">avilavate123@gmail.com</div>
+              <div className="text-center">
+                <a href="http://github.com/avilavate">Avil Avate (GitHub)</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
